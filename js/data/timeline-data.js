537bb768d27de5d195e67eb34b0491aa5f3f3d85
arsyayaayaa <a.rsyad96.242@gmail.com>
2026-07-16 09:14:12 +0800

    Create timeline-data.js

---
+++ js/data/timeline-data.js
@@ -0,0 +1,80 @@
+/**
+ * ==================================================
+ * Project Hanami
+ * Data : Timeline
+ * Description : Journey timeline content.
+ * Version : 1.0
+ * ==================================================
+ */
+
+const TIMELINE = [
+
+    {
+        id: 1,
+
+        date: "12 July 2023",
+
+        title: "Our First Meeting",
+
+        description:
+            "An ordinary day unexpectedly became the beginning of one of the most meaningful journeys in my life."
+    },
+
+    {
+        id: 2,
+
+        date: "18 August 2023",
+
+        title: "First Long Conversation",
+
+        description:
+            "Hours passed so quickly while we shared stories, dreams, and laughter without realizing how special the moment was."
+    },
+
+    {
+        id: 3,
+
+        date: "5 October 2023",
+
+        title: "Our First Photo",
+
+        description:
+            "Just one photo, yet it became a memory that always brings a smile whenever I look back."
+    },
+
+    {
+        id: 4,
+
+        date: "25 December 2023",
+
+        title: "A Wonderful Day",
+
+        description:
+            "Some days don't need to be extraordinary to become unforgettable. Being together was already enough."
+    },
+
+    {
+        id: 5,
+
+        date: "14 February 2024",
+
+        title: "A Moment to Remember",
+
+        description:
+            "A small moment that quietly became one of the memories I treasure the most."
+    },
+
+    {
+        id: 6,
+
+        date: "Today",
+
+        title: "Happy Birthday",
+
+        description:
+            "This isn't the end of our story. It's simply another beautiful chapter waiting to be written together."
+    }
+
+];
+
+export default TIMELINE;
