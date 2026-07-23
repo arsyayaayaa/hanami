2766c672c6757366baca476d4f14a4773d7dd89b
arsyayaayaa <a.rsyad96.242@gmail.com>
2026-07-23 09:59:31 +0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/**
+ * ==================================================
+ * Project : Hanami
+ * File    : Main
+ * Description : Application entry point.
+ * Version : 1.0
+ * ==================================================
+ */
+
+import app from "./core/app.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    app.init();
+
+});
