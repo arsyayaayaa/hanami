afb00957ea94a91b593cc13223d1c81afade06b4
arsyayaayaa <a.rsyad96.242@gmail.com>
2026-07-23 11:10:35 +0800

    Update firestore.js

--- firebase/firestore.js
+++ firebase/firestore.js
@@ -1,1 +1,26 @@
+/**
+ * ==================================================
+ * Project : Hanami
+ * Module  : Firestore
+ * Description : Create Firestore instance.
+ * Version : 1.0
+ * ==================================================
+ */
 
+import { getFirestore } from "firebase/firestore";
+
+import firebaseApp from "./firebase-config.js";
+
+/**
+ * ------------------------------------------
+ * Firestore Database
+ * ------------------------------------------
+ */
+
+const db = getFirestore(
+
+    firebaseApp
+
+);
+
+export default db;
